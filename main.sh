52eaa9c93592538c95db5946a29a0176c4d1fa8d
capoony <capoony@gmail.com>
2024-11-25 09:01:15 +0100

    updates

--- main.sh
+++ main.sh
@@ -84,33 +84,102 @@ python -m http.server 8000
 ### The Output HTML should open automatically on LINUX.
 ### On a MAC, copy the PATH to the HTML and copy into your Browser
 
-## ----> What do you see?
+## ----> What do you see? (Investigate HTML and Output folder)
 ## ----> Why are there no additional haplotypes in diploid loci? (Check the tabular summary of the Consensus Seq reconstruction)
-## ----> Are the phylogenies of all loci consistent?
+## ----> Are the phylogenies of all loci consistent? (Compare Phylogenies of single loci and of Super trees)
+
+# (5) What happens, if we include ALL consensus sequences?
+
+## execute Pipeline
+bash ${AMPLIPIPER}/shell/AmpliPiper.sh \
+    --samples ${AMPLIPIPER}/testdata/data/samples.csv \
+    --primers ${AMPLIPIPER}/testdata/data/primers.csv \
+    --output ${WD}/results/Syrphid/AllCons \
+    --quality 10 \
+    --nreads 1000 \
+    --blast your@email.com \
+    --similar_consensus 97 \
+    --threads 16 \
+    --kthreshold 0.05 \
+    --minreads 50 \
+    --sizerange 100 \
+    --outgroup He_mor_41 \
+    --force \
+    --freqthreshold 0
 
 # ======================================== Part II ===========================================
 
-## make input file for the samples
-echo \
-    """ABOL24-0001.1,${WD}/data/Opiliones/raw_reads/barcode21.fastq.gz
-ABOL24-0005,${WD}/data/Opiliones/raw_reads/barcode33.fastq.gz
-ABOL24-0009,${WD}/data/Opiliones/raw_reads/barcode45.fastq.gz
-ABOL24-0011,${WD}/data/Opiliones/raw_reads/barcode57.fastq.gz
-ABOL24-0016,${WD}/data/Opiliones/raw_reads/barcode69.fastq.gz
-ABOL24-0017,${WD}/data/Opiliones/raw_reads/barcode81.fastq.gz
-ABOL24-0019,${WD}/data/Opiliones/raw_reads/barcode93.fastq.gz
-ABOL24-0020,${WD}/data/Opiliones/raw_reads/barcode10.fastq.gz
-""" >${WD}/data/Opiliones/files/samples.csv
+# In the second part, we will investigate some more complex datasets
 
-### run AmpliPiper
+# (1) Opiliones
+
+## (a) generate samples file
+echo \
+    """ABOL24-0001.1,${WD}/datasets/Opiliones/data/reads/barcode21.fastq.gz
+ABOL24-0005,${WD}/datasets/Opiliones/data/reads/barcode33.fastq.gz
+ABOL24-0009,${WD}/datasets/Opiliones/data/reads/barcode45.fastq.gz
+ABOL24-0011,${WD}/datasets/Opiliones/data/reads/barcode57.fastq.gz
+ABOL24-0016,${WD}/datasets/Opiliones/data/reads/barcode69.fastq.gz
+ABOL24-0017,${WD}/datasets/Opiliones/data/reads/barcode81.fastq.gz
+ABOL24-0019,${WD}/datasets/Opiliones/data/reads/barcode93.fastq.gz
+ABOL24-0020,${WD}/datasets/Opiliones/data/reads/barcode10.fastq.gz
+""" >${WD}/datasets/Opiliones/files/samples.csv
+
+## (b) run AmpliPiper
+
+## modify parameters if you wish
+
 bash /app/shell/AmpliPiper.sh \
-    --samples ${WD}/data/Opiliones/files/samples.csv \
-    --primers ${WD}/data/Opiliones/files/primers.csv \
-    --output ${WD}/Opiliones/results/demo/ \
+    --samples ${WD}/datasets/Opiliones/data/samples.csv \
+    --primers ${WD}/datasets/Opiliones/data/primers.csv \
+    --output ${WD}/datasets/Opiliones/results/demo1/ \
     --quality 10 \
     --nreads 1000 \
-    --threads 100 \
-    --kthreshold 0.1 \
+    --threads 16 \
+    --kthreshold 0.05 \
+    --blast your@email.com \
+    --similar_consensus 97 \
+    --minreads 50 \
+    --sizerange 100 \
+    --freqthreshold 0 \
+    --force
+
+## ----> What do you see? (Investigate HTML and Output folder)
+## ----> How do you interpret?
+## ----> repeat with other parameter combinations
+
+# (2) Mercurialis
+
+## (a) generate samples file
+
+## print header for samples.csv
+printf "ID,PATH\n" >${WD}/datasets/Mercurialis/data/samples.csv
+
+## loop through input FASTQ files
+for Filepath in ${WD}/datasets/Mercurialis/data/reads/*fastq.gz; do
+
+    ## get Filename
+    Filename=${Filepath##*/}
+
+    ## get File ID
+    ID=${Filename%.fastq.gz*}
+
+    ## print to samples.csv
+    echo ${ID},${Filepath} >>${WD}/datasets/Mercurialis/data/samples.csv
+done
+
+## (b) run AmpliPiper
+
+## modify parameters if you wish
+
+bash /app/shell/AmpliPiper.sh \
+    --samples ${WD}/datasets/Mercurialis/data/samples.csv \
+    --primers ${WD}/datasets/Mercurialis/data/primers.csv \
+    --output ${WD}/datasets/Mercurialis/results/demo1/ \
+    --quality 10 \
+    --nreads 1000 \
+    --threads 16 \
+    --kthreshold 0.05 \
     --blast your@email.com \
     --similar_consensus 97 \
     --minreads 50 \
